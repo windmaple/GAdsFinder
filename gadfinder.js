ec72e7222a53f6089bbedca806bb66131558195a
Wei Wei <windmaple@gmail.com>
2016-02-23 13:03:15 +0800

    Added panel/ folder

--- gadfinder.js
+++ gadfinder.js
@@ -1,7 +1,7 @@
 chrome.devtools.panels.create(
     'GoogleAdFinder',
     null, // No icon path
-    'refreshForGoogleAd.html',
+    'panel/refreshForGoogleAd.html',
     null // no callback needed
 );
 
