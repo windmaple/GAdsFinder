ef5b7738f3a8fd843995dfacc2094aee2d208a98
Wei Wei <windmaple@gmail.com>
2016-02-23 16:32:25 +0800

    Got the listener working, but same problem persists:
    Main thread can't be stopped. Looking to event page.

--- panel/refreshForGoogleAd.js
+++ panel/refreshForGoogleAd.js
@@ -1,2 +1,25 @@
 
+var found = false;
+
+chrome.devtools.network.onRequestFinished.addListener(function (request) {
+  for(var i=0; i<request.response.headers.length; i++) {
+//    alert(request.response.headers[i].value);
+    if(request.response.headers[i].value.indexOf("client=ca-pub-") != -1 && found == false) {
+      alert("found; stopping refreshing");
+      found = true;
+    }
+  }
+});
+
+var options = {
+  ignoreCache: true,
+  userAgent: null,
+  injectedScript: null,
+  preprocessingScript: null
+};
+
+for(var i=0; i<10; i++) {
+  if(!found)
+    chrome.devtools.inspectedWindow.reload(options);
+}
 
