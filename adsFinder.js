ce6b2b1c7ae76a0f7f3a69b3bba7ba4bd9f4fc6a
Wei Wei <windmaple@gmail.com>
2016-03-30 20:29:54 +0800

    pretty ugly hack to get the counter updated

--- adsFinder.js
+++ adsFinder.js
@@ -1,6 +1,29 @@
+var view_counts = 1;
+var windowObj;
+var pubURL;
+
 chrome.devtools.panels.create(
-    'adsFinder',
-    null, // No icon path
-    'panel/refreshForAd.html',
-    null // no callback needed
+  'adsFinder',
+  null, // No icon path
+  'panel/refreshForAd.html',
+  function(panel){
+    panel.onShown.addListener(function tmp(panel_window){
+      panel.onShown.removeListner(tmp);
+      windowObj = panel_window;
+    });
+  }
+);
+
+
+
+chrome.devtools.network.onRequestFinished.addListener(
+  function(entries) {
+    if (!pubURL) {
+      pubURL = entries.request.url;
+    }
+    else if(entries.request.url == pubURL){
+      view_counts++;
+      windowObj.document.getElementById('count').innerHTML = view_counts;
+    }
+  }
 );
